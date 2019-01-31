c69a423b9b3120118477afe9a0607d756f5d918d
Chris Rowe <chris@chrisrowe.co.uk>
2019-01-31 20:39:01 +0000

    Added Firefox user configuration

---
+++ mozilla/firefox/user.default/user.js
@@ -0,0 +1,6 @@
+// Fix invisible text on dark themes
+user_pref("widget.chrome.allow-gtk-dark-theme", true);
+user_pref("widget.content.allow-gtk-dark-theme", true);
+
+// Fix HTML5 video tearing
+user_pref("layers.acceleration.force-enabled", true);
